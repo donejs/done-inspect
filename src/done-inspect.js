75222348013bd0beab1fbe5e87170d45dafeb343
Mark Stahl <mjstahl@gmail.com>
2017-05-03 17:01:24 -0600

    Basic component markup

--- src/done-inspect.js
+++ src/done-inspect.js
@@ -1,3 +1,23 @@
-export default function() {
-  return `This is the done-inspect plugin`;
-}
+import Component from 'can-component';
+import DefineMap from 'can-define/map/map';
+import stache from 'can-stache';
+import view from './done-inspect.stache';
+import './styles/done-inspect.less';
+
+export const viewModel = DefineMap.extend({
+  expanded: {
+    type: 'boolean',
+    value: false,
+  },
+  toggleExpanded(ev) {
+    ev.preventDefault();
+    ev.stopPropagation();
+    this.expanded = !this.expanded;
+  },
+});
+
+export default Component.extend({
+  tag: 'done-inspect',
+  viewModel,
+  view,
+});
