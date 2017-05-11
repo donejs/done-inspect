e0210381a1cf85ad621e888a2d48d42a4add4b62
Mark Stahl <mjstahl@gmail.com>
2017-05-11 11:59:03 -0400

    Update spacing. Add index.html to redirect to demo

--- build.js
+++ build.js
@@ -1,18 +1,16 @@
 var stealTools = require("steal-tools");
 
 stealTools.export({
-	steal: {
-		config: __dirname + "/package.json!npm"
-	},
-	outputs: {
-		"+cjs": {},
-		"+amd": {},
-		"+global-js": {}
-	}
+  steal: {
+    config: __dirname + "/package.json!npm"
+  },
+  outputs: {
+    "+cjs": {},
+    "+amd": {},
+    "+global-js": {}
+  }
 }).catch(function(e){
-	
-	setTimeout(function(){
-		throw e;
-	},1);
-	
+  setTimeout(function(){
+    throw e;
+  },1);
 });
