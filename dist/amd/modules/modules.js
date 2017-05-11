529f03b3c07164f754d408c42f6bfb6ffe472dc1
Mark Stahl <mjstahl@gmail.com>
2017-05-11 16:25:34 -0400

    0.0.2

---
+++ dist/amd/modules/modules.js
@@ -0,0 +1,13 @@
+/*done-inspect@0.0.1#modules/modules*/
+define([
+    'exports',
+    './show-names/show-names'
+], function (exports, _showNames) {
+    'use strict';
+    Object.defineProperty(exports, '__esModule', { value: true });
+    var _showNames2 = _interopRequireDefault(_showNames);
+    function _interopRequireDefault(obj) {
+        return obj && obj.__esModule ? obj : { default: obj };
+    }
+    exports.default = [_showNames2.default];
+});
---
+++ dist/cjs/modules/modules.js
@@ -0,0 +1,9 @@
+/*done-inspect@0.0.1#modules/modules*/
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+var _showNames = require('./show-names/show-names.js');
+var _showNames2 = _interopRequireDefault(_showNames);
+function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : { default: obj };
+}
+exports.default = [_showNames2.default];
