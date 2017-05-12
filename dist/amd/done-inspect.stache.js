7748fb84a1383aa9e09d0d20d5ef196fe0ce98e3
Mark Stahl <mjstahl@gmail.com>
2017-05-12 00:12:37 -0400

    0.0.5

---
+++ dist/amd/done-inspect.stache.js
@@ -0,0 +1,342 @@
+/*done-inspect@0.0.4#done-inspect.stache!steal-stache@3.0.5#steal-stache*/
+define([
+    'module',
+    'can-stache',
+    'can-stache/src/mustache_core',
+    'can-view-import@3.0.7#can-view-import',
+    'can-stache-bindings@3.0.19#can-stache-bindings'
+], function (module, stache, mustacheCore) {
+    var renderer = stache([
+        {
+            'tokenType': 'start',
+            'args': [
+                'div',
+                false
+            ]
+        },
+        {
+            'tokenType': 'attrStart',
+            'args': ['id']
+        },
+        {
+            'tokenType': 'attrValue',
+            'args': ['done-inspect-toolbar']
+        },
+        {
+            'tokenType': 'attrEnd',
+            'args': ['id']
+        },
+        {
+            'tokenType': 'attrStart',
+            'args': ['class']
+        },
+        {
+            'tokenType': 'attrValue',
+            'args': ['done-inspect done-inspect-toolbar ']
+        },
+        {
+            'tokenType': 'special',
+            'args': ['#if expanded']
+        },
+        {
+            'tokenType': 'attrValue',
+            'args': ['expanded']
+        },
+        {
+            'tokenType': 'special',
+            'args': ['/if']
+        },
+        {
+            'tokenType': 'attrEnd',
+            'args': ['class']
+        },
+        {
+            'tokenType': 'attrStart',
+            'args': ['role']
+        },
+        {
+            'tokenType': 'attrValue',
+            'args': ['region']
+        },
+        {
+            'tokenType': 'attrEnd',
+            'args': ['role']
+        },
+        {
+            'tokenType': 'attrStart',
+            'args': ['aria-expanded']
+        },
+        {
+            'tokenType': 'special',
+            'args': ['expanded']
+        },
+        {
+            'tokenType': 'attrEnd',
+            'args': ['aria-expanded']
+        },
+        {
+            'tokenType': 'end',
+            'args': [
+                'div',
+                false
+            ]
+        },
+        {
+            'tokenType': 'chars',
+            'args': ['\n  ']
+        },
+        {
+            'tokenType': 'start',
+            'args': [
+                'div',
+                false
+            ]
+        },
+        {
+            'tokenType': 'attrStart',
+            'args': ['class']
+        },
+        {
+            'tokenType': 'attrValue',
+            'args': ['done-inspect-toolbar-body']
+        },
+        {
+            'tokenType': 'attrEnd',
+            'args': ['class']
+        },
+        {
+            'tokenType': 'end',
+            'args': [
+                'div',
+                false
+            ]
+        },
+        {
+            'tokenType': 'chars',
+            'args': ['\n    ']
+        },
+        {
+            'tokenType': 'start',
+            'args': [
+                'ul',
+                false
+            ]
+        },
+        {
+            'tokenType': 'attrStart',
+            'args': ['class']
+        },
+        {
+            'tokenType': 'attrValue',
+            'args': ['done-inspect-plugins']
+        },
+        {
+            'tokenType': 'attrEnd',
+            'args': ['class']
+        },
+        {
+            'tokenType': 'end',
+            'args': [
+                'ul',
+                false
+            ]
+        },
+        {
+            'tokenType': 'chars',
+            'args': ['\n      ']
+        },
+        {
+            'tokenType': 'special',
+            'args': ['{renderedGroups']
+        },
+        {
+            'tokenType': 'chars',
+            'args': ['\n    ']
+        },
+        {
+            'tokenType': 'close',
+            'args': ['ul']
+        },
+        {
+            'tokenType': 'chars',
+            'args': ['\n  ']
+        },
+        {
+            'tokenType': 'close',
+            'args': ['div']
+        },
+        {
+            'tokenType': 'chars',
+            'args': ['\n  ']
+        },
+        {
+            'tokenType': 'start',
+            'args': [
+                'button',
+                false
+            ]
+        },
+        {
+            'tokenType': 'attrStart',
+            'args': ['aria-controls']
+        },
+        {
+            'tokenType': 'attrValue',
+            'args': ['done-inspect-toolbar']
+        },
+        {
+            'tokenType': 'attrEnd',
+            'args': ['aria-controls']
+        },
+        {
+            'tokenType': 'attrStart',
+            'args': ['class']
+        },
+        {
+            'tokenType': 'attrValue',
+            'args': ['done-inspect-toolbar-toggle']
+        },
+        {
+            'tokenType': 'attrEnd',
+            'args': ['class']
+        },
+        {
+            'tokenType': 'attrStart',
+            'args': ['aria-label']
+        },
+        {
+            'tokenType': 'attrValue',
+            'args': ['[done-inspect] Toggle menu']
+        },
+        {
+            'tokenType': 'attrEnd',
+            'args': ['aria-label']
+        },
+        {
+            'tokenType': 'attrStart',
+            'args': ['($click)']
+        },
+        {
+            'tokenType': 'attrValue',
+            'args': ['toggleExpanded(%event)']
+        },
+        {
+            'tokenType': 'attrEnd',
+            'args': ['($click)']
+        },
+        {
+            'tokenType': 'end',
+            'args': [
+                'button',
+                false
+            ]
+        },
+        {
+            'tokenType': 'chars',
+            'args': ['\n    ']
+        },
+        {
+            'tokenType': 'start',
+            'args': [
+                'div',
+                false
+            ]
+        },
+        {
+            'tokenType': 'attrStart',
+            'args': ['aria-hidden']
+        },
+        {
+            'tokenType': 'attrValue',
+            'args': ['true']
+        },
+        {
+            'tokenType': 'attrEnd',
+            'args': ['aria-hidden']
+        },
+        {
+            'tokenType': 'attrStart',
+            'args': ['class']
+        },
+        {
+            'tokenType': 'attrValue',
+            'args': ['done-inspect-toolbar-logo']
+        },
+        {
+            'tokenType': 'attrEnd',
+            'args': ['class']
+        },
+        {
+            'tokenType': 'end',
+            'args': [
+                'div',
+                false
+            ]
+        },
+        {
+            'tokenType': 'chars',
+            'args': ['\n      ']
+        },
+        {
+            'tokenType': 'start',
+            'args': [
+                'img',
+                true
+            ]
+        },
+        {
+            'tokenType': 'attrStart',
+            'args': ['src']
+        },
+        {
+            'tokenType': 'attrValue',
+            'args': ['https://raw.githubusercontent.com/donejs/done-inspect/master/src/images/donejs-icon-full-color.png']
+        },
+        {
+            'tokenType': 'attrEnd',
+            'args': ['src']
+        },
+        {
+            'tokenType': 'end',
+            'args': [
+                'img',
+                true
+            ]
+        },
+        {
+            'tokenType': 'chars',
+            'args': ['\n    ']
+        },
+        {
+            'tokenType': 'close',
+            'args': ['div']
+        },
+        {
+            'tokenType': 'chars',
+            'args': ['\n  ']
+        },
+        {
+            'tokenType': 'close',
+            'args': ['button']
+        },
+        {
+            'tokenType': 'chars',
+            'args': ['\n']
+        },
+        {
+            'tokenType': 'close',
+            'args': ['div']
+        },
+        {
+            'tokenType': 'done',
+            'args': []
+        }
+    ]);
+    return function (scope, options, nodeList) {
+        var moduleOptions = { module: module };
+        if (!(options instanceof mustacheCore.Options)) {
+            options = new mustacheCore.Options(options || {});
+        }
+        return renderer(scope, options.add(moduleOptions), nodeList);
+    };
+});
